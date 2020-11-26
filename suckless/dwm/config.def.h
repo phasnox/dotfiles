cbbb04e18c16b0264e33085882821bc2973ee01f
Guillermo Castillo <phasnox@gmail.com>
2020-11-26 16:48:05 -0500

    fix fonts

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -5,9 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "AurulentSansMono Nerd Font" };
-
-static char *font[] = {
+static char *fonts[] = {
     "AurulentSansMono Nerd Font:pixelsize=17:antialias=true:autohint=true",
     "Font Awesome 5 Pro:pixelsize=17:antialias=true:autohint=true"
 };
